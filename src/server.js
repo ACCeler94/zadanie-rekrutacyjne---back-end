784d3361bda9cda1d67f27b5e5bfa522ad9bfd84
Artur Celer <acceler945@gmail.com>
2025-05-30 17:06:20 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
+app.use((err, req, res, _next) => {
+  const message = err.message || 'Internal server error.';
+  res.status(500).json({ message });
+});
+
+app.listen(port, () => {
+  console.log(`[Server]: I am running at http://localhost:${port}`);
+});
