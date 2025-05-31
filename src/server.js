842379f919884bcc52c8f0e53c3c1d91d449f1af
Artur Celer <acceler945@gmail.com>
2025-05-31 19:05:05 +0200

    Add timeout middleware, add routes. Refactor: improve error messages for consistency

--- src/server.js
+++ src/server.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import './config/env.js';
 import updateOrdersDaily from './jobs/updateOrdersDaily.js';
+import { orderRoutes } from './routes/orders.routes.js';
+import timeout from 'connect-timeout';
 
 
 const app = express();
 const port = 8000;
 
+// Global timeout middleware
+app.use(timeout('20s'));
+
+// Handle timeout error
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
+app.use('/api', orderRoutes)
+
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).json({ error: 'Endpoint not found.' });
 });
 
 app.use((err, req, res, _next) => {
   const message = err.message || 'Internal server error.';
-  res.status(500).json({ message });
+  res.status(500).json({ error: message });
 });
 
 // Create crone-job to update orders daily
